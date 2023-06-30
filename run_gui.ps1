1d26f70ae0aff6b1a7e5b90947bf31ef27e1640b
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-06-30 17:35:56 +0800

    Create run_gui.ps1

---
+++ run_gui.ps1
@@ -0,0 +1,14 @@
+$execution-provider="cuda"
+$execution-threads=8
+$video-quality=1
+
+Set-Location $PSScriptRoot
+.\venv\Scripts\activate
+
+$Env:HF_HOME = "./huggingface"
+
+python.exe run.py `
+--frame-processor face_swapper face_enhancer `
+--execution-provider $execution-provider `
+--execution-threads=$execution-threads `
+--video-quality=$video-quality
