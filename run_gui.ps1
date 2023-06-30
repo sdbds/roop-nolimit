7fc2e10186e964ef4062f8031dfc88b9e827fa45
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-06-30 18:18:10 +0800

    Update run_gui.ps1

--- run_gui.ps1
+++ run_gui.ps1
@@ -1,6 +1,6 @@
-$execution-provider="cuda"
-$execution-threads=8
-$video-quality=1
+$execution_provider="cuda"
+$execution_threads=8
+$video_quality=1
 
 Set-Location $PSScriptRoot
 .\venv\Scripts\activate
@@ -9,6 +9,6 @@ $Env:HF_HOME = "./huggingface"
 
 python.exe run.py `
 --frame-processor face_swapper face_enhancer `
---execution-provider=$execution-provider `
---execution-threads=$execution-threads `
---video-quality=$video-quality
+--execution-provider=$execution_provider `
+--execution-threads=$execution_threads `
+--video-quality=$video_quality
